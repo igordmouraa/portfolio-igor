4bc834266d6e88c3a38feb0b933258ff542b3c8b
igordmouraa <igordmoura_@hotmail.com>
2025-02-18 18:24:39 -0300

    feat: Add KnowTechs component to Home page
    Integrated the KnowTechs component into the Home page layout. This enhances the page by providing additional content related to technology expertise.

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import {HeroSection} from "@/app/components/pagess/home/hero_section";
+import {KnowTechs} from "@/app/components/pagess/home/know_techs";
 
 export default async function Home() {
   return (
     <>
         <HeroSection/>
-
+        <KnowTechs/>
     </>
   )
 }
