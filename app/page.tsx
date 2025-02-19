c8e932a81f969d8bfb528035833de3ae0a8ad251
igordmouraa <igordmoura_@hotmail.com>
2025-02-19 17:50:45 -0300

    feat: Add HighlightedProjects component to Home page
    Integrates the HighlightedProjects component into the Home page, enhancing its content structure. Also adds the `typewriter-effect` package to `package.json`, likely in preparation for functionality related to the new component.

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import {HeroSection} from "@/app/components/pagess/home/hero_section";
 import {KnowTechs} from "@/app/components/pagess/home/know_techs";
+import {HighlightedProjects} from "@/app/components/pagess/home/highlighted-projects";
 
 export default async function Home() {
   return (
     <>
         <HeroSection/>
         <KnowTechs/>
+        <HighlightedProjects/>
     </>
   )
 }
