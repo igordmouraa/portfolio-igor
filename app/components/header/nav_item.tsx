26f3c0f7469377e5194bfc8cb7885bc6e63d2ec6
igordmouraa <igordmoura_@hotmail.com>
2025-02-19 23:58:39 -0300

    feat: Add animations and hover effects to NavItem component
    Introduced Framer Motion for animations, including fade-in and active underline effects. Enhanced user experience with smoother hover transitions and better visual feedback for active links.

--- app/components/header/nav_item.tsx
+++ app/components/header/nav_item.tsx
@@ -1,22 +1,41 @@
 'use client'
+import { motion } from "framer-motion";
 import Link from "next/link";
-import {usePathname} from "next/navigation";
-import {cn} from "@/app/lib/utils";
+import { usePathname } from "next/navigation";
+import { cn } from "@/app/lib/utils";
 
 type NavItemProps = {
-    label: string
-    href: string
-}
+    label: string;
+    href: string;
+    delay?: number;
+};
 
-export const NavItem = ({label, href} : NavItemProps) =>{
-    const pathName =  usePathname();
-
+export const NavItem = ({ label, href, delay = 0 }: NavItemProps) => {
+    const pathName = usePathname();
     const isActive = pathName === href;
 
     return (
-        <Link href={href} className={cn('text-gray-400 flex items-center gap-2 font-medium font-mono', isActive && 'text-gray-50')}>
-            <span className='text-purple-600'>#</span>
-            {label}
-        </Link>
-    )
-}
+        <motion.div
+            initial={{ y: -20, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ delay }}
+        >
+            <Link
+                href={href}
+                className={cn(
+                    "text-gray-400 flex items-center gap-2 font-medium font-mono hover:text-gray-50 transition-colors relative",
+                    isActive && "text-gray-50"
+                )}
+            >
+                <span className="text-purple-600">#</span>
+                {label}
+                {isActive && (
+                    <motion.span
+                        layoutId="underline"
+                        className="absolute left-0 top-full mt-1 h-[2px] w-full bg-purple-600"
+                    />
+                )}
+            </Link>
+        </motion.div>
+    );
+};
