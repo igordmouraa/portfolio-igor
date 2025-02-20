18d59d669e01a7170d953c2c347f5c6ae3e03995
igordmouraa <igordmoura_@hotmail.com>
2025-02-20 00:21:04 -0300

    fix: Add support for anchor elements in Button component
    Refactored the Button component to handle both button and anchor elements by extending its props. Introduced conditional rendering for `href` to support `next/link` for anchor elements. This improves versatility and usability of the Button component.

--- app/components/button/index.tsx
+++ app/components/button/index.tsx
@@ -1,16 +1,31 @@
-import {ButtonHTMLAttributes} from "react";
-import {cn} from "@/app/lib/utils";
+import { ButtonHTMLAttributes, AnchorHTMLAttributes } from "react";
+import Link from "next/link";
+import { cn } from "@/app/lib/utils";
 
-type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+type ButtonProps = (ButtonHTMLAttributes<HTMLButtonElement> | AnchorHTMLAttributes<HTMLAnchorElement>) & {
+    href?: string;
+};
 
-export const Button = ({children, className, ...props}: ButtonProps) => {
+export const Button = ({ children, className, href, ...props }: ButtonProps) => {
+    const combinedClasses = cn(
+        "bg-purple-600 text-gray-50 px-4 py-3 rounded-lg flex items-center justify-center gap-2 hover:bg-purple-950 transition-all disabled:opacity-50",
+        className
+    );
+
+    if (href) {
+        return (
+            <Link href={href} passHref legacyBehavior>
+                <a className={combinedClasses} {...(props as AnchorHTMLAttributes<HTMLAnchorElement>)}>
+                    {children}
+                </a>
+            </Link>
+        );
+    }
+
     return (
         <button
-            className={cn(
-                "bg-purple-600 text-gray-50 px-4 py-3 rounded-lg flex items-center justify-center gap-2 hover:bg-purple-950 transition-all disabled:opacity-50",
-                className
-            )}
-            {...props}
+            className={combinedClasses}
+            {...(props as ButtonHTMLAttributes<HTMLButtonElement>)}
         >
             {children}
         </button>
