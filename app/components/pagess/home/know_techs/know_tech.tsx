9218806e69357ef045ac289ddac9b656898c28ea
igordmouraa <igordmoura_@hotmail.com>
2025-02-20 23:51:51 -0300

    feat: Add hover animations to KnowTech using framer-motion
    Replaced the static div wrapper with a motion.div to introduce smooth scaling and shadow effects on hover. This enhances the user experience by providing better visual feedback during interactions.

--- app/components/pagess/home/know_techs/know_tech.tsx
+++ app/components/pagess/home/know_techs/know_tech.tsx
@@ -1,4 +1,5 @@
 import {ReactNode} from "react";
+import { motion } from 'framer-motion'
 import {getRelativeTimeString} from "@/app/utils/get-relative-time";
 
 type KnowTechProps = {
@@ -10,18 +11,20 @@ type KnowTechProps = {
     className?: string;
 }
 
-export const KnowTech = ({tech}: KnowTechProps) => {
-    const relativeTime = getRelativeTimeString(new Date(tech.startDate), 'pt-BR',
-        ).replace('há', '')
+export const KnowTech = ({ tech }: KnowTechProps) => {
+    const relativeTime = getRelativeTimeString(new Date(tech.startDate), 'pt-BR').replace('há', '');
 
     return (
-        <div className='p-6 rounded-lg bg-gray-600/20 text-gray-500 flex flex-col hover:text-purple-500 hover:bg-gray-600/30'>
-            <div className='flex items-center justify-between '>
-                <p className={'font-medium'}>{tech.name}</p>
+        <motion.div
+            className='p-6 rounded-lg bg-gray-600/20 text-gray-500 flex flex-col hover:text-purple-500 hover:bg-gray-600/30 transition-all duration-300 ease-in-out'
+            whileHover={{ scale: 1.05, boxShadow: '0px 10px 30px rgba(0, 0, 0, 0.2)' }}
+            transition={{ type: 'spring', stiffness: 300 }}
+        >
+            <div className='flex items-center justify-between'>
+                <p className='font-medium'>{tech.name}</p>
                 {tech.icon}
             </div>
             <span>{relativeTime} de experiência</span>
-
-        </div>
-    )
+        </motion.div>
+    );
 }
