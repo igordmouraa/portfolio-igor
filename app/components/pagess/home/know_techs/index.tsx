ec30958a188b74d9817219aa4f1a948dd32136de
igordmouraa <igordmoura_@hotmail.com>
2025-05-22 11:07:57 -0300

    fix: Update technology list in index.tsx for accuracy and consistency

--- app/components/pagess/home/know_techs/index.tsx
+++ app/components/pagess/home/know_techs/index.tsx
@@ -36,21 +36,21 @@ const itemVariants = {
 
 const techs = [
     { icon: <SiNodedotjs />, name: 'Node.js', startDate: '2023-03-10' },
-    { icon: <TbBrandTypescript />, name: 'Typescript', startDate: '2024-05-10' },
     { icon: <TbBrandJavascript />, name: 'Javascript', startDate: '2023-03-10' },
-    { icon: <TbBrandFirebase />, name: 'Firebase', startDate: '2024-05-10' },
-    { icon: <TbBrandMongodb />, name: 'Mongo DB', startDate: '2023-07-10' },
-    { icon: <TbBrandMysql />, name: 'MySQL', startDate: '2023-07-10' },
     { icon: <SiNestjs />, name: 'Nest.js', startDate: '2024-03-10' },
-    { icon: <TbBrandDocker />, name: 'Docker', startDate: '2024-03-10' },
+    { icon: <TbBrandTypescript />, name: 'Typescript', startDate: '2024-05-10' },
     { icon: <TbBrandReact />, name: 'React', startDate: '2023-07-10' },
     { icon: <TbBrandNextjs />, name: 'Next.js', startDate: '2024-05-10' },
     { icon: <TbBrandTailwind />, name: 'Tailwind CSS', startDate: '2023-07-10' },
     { icon: <TbBrandReactNative />, name: 'React Native', startDate: '2024-02-20' },
-    { icon: <TbBrandGit />, name: 'Git', startDate: '2023-03-10' },
+    { icon: <TbBrandMongodb />, name: 'Mongo DB', startDate: '2023-07-10' },
+    { icon: <TbBrandMysql />, name: 'MySQL', startDate: '2023-07-10' },
+    { icon: <TbBrandFirebase />, name: 'Firebase', startDate: '2024-05-10' },
     { icon: <TbBrandVercel />, name: 'Vercel', startDate: '2024-07-10' },
+    { icon: <TbBrandFigma />, name: 'Figma', startDate: '2023-03-20' },
     { icon: <SiGooglecloud />, name: 'Google Cloud', startDate: '2024-02-20' },
-    { icon: <TbBrandFigma />, name: 'Figma', startDate: '2023-03-20' }
+    { icon: <TbBrandDocker />, name: 'Docker', startDate: '2024-03-10' },
+    { icon: <TbBrandGit />, name: 'Git', startDate: '2023-03-10' },
 ]
 
 export const KnowTechs = () => {
