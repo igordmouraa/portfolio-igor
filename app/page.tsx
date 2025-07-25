89f4b5ec92344c9e9b91bd0e583033d2aecf0f0d
igordmouraa <igordmoura_@hotmail.com>
2025-07-24 23:24:52 -0300

    feat: Refactor Home component layout to include HorizontalDivider for improved section separation

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,17 @@ import {HeroSection} from "@/app/components/pagess/home/hero_section";
 import {KnowTechs} from "@/app/components/pagess/home/know_techs";
 import {HighlightedProjects} from "@/app/components/pagess/home/highlighted-projects";
 import {ProfessionalExperience} from "@/app/components/pagess/home/professional-experience";
+import {HorizontalDivider} from "@/app/components/divider";
 
 export default async function Home() {
-  return (
-    <>
-        <HeroSection/>
-        <KnowTechs/>
-        <HighlightedProjects/>
-        <ProfessionalExperience/>
-    </>
-  )
+    return (
+        <main>
+            <div id="home"><HeroSection/></div>
+            <div id="techs"><KnowTechs/></div>
+            <HorizontalDivider className="my-0"/>
+            <div id="projects"><HighlightedProjects/></div>
+            <HorizontalDivider className="my-0"/>
+            <div id="experience"><ProfessionalExperience/></div>
+        </main>
+    )
 }
