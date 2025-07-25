84120ede66b6da2d7df1f5c2ba8753d3a4e8fe89
igordmouraa <igordmoura_@hotmail.com>
2025-07-24 23:24:35 -0300

    feat: Add animation to HorizontalDivider component using Framer Motion

--- app/components/divider/index.tsx
+++ app/components/divider/index.tsx
@@ -1,11 +1,21 @@
-import { cn } from '@/app/lib/utils'
+'use client'
+
+import {motion} from 'framer-motion';
+import {cn} from "@/app/lib/utils";
 
 type HorizontalDividerProps = {
     className?: string
 }
 
-export const HorizontalDivider = ({ className }: HorizontalDividerProps) => {
+export const HorizontalDivider = ({className}: HorizontalDividerProps) => {
     return (
-        <div className={cn('w-full my-8 border-b border-b-gray-800', className)} />
+        <motion.div
+            className={cn('w-full my-8 border-b border-b-gray-800', className)}
+            initial={{opacity: 0, scaleX: 0}}
+            whileInView={{opacity: 1, scaleX: 1}}
+            transition={{duration: 0.5, ease: 'easeOut'}}
+            viewport={{once: true}}
+            style={{transformOrigin: 'center'}}
+        />
     )
 }
